9cdf2d87973090d9b5c3e6c70bf3295873255295
Ezequiel Coronel <nikocor99@gmail.com>
2021-10-27 17:27:58 -0300

    Added playRound function and the lose cases

--- script.js
+++ script.js
@@ -2,5 +2,23 @@ let valuesArray = ["Rock", "Paper", "Scissors"];
 
 function computerPlay() {
   let value = Math.floor(Math.random() * 3);
-  console.log(valuesArray[value]);
+  return valuesArray[value];
 }
+
+function playRound(playerSelection, computerSelection) {
+  playerSelection = playerSelection.toLowerCase();
+  computerSelection = computerSelection.toLowerCase();
+
+  if (
+    (computerSelection == "rock" && playerSelection == "scissors") ||
+    (computerSelection == "scissors" && playerSelection == "paper") ||
+    (computerSelection == "paper" && playerSelection == "rock")
+  ) {
+    return `You Lose! ${computerSelection} beats ${playerSelection}`;
+  }
+}
+
+const playerSelection = "rock";
+const computerSelection = computerPlay();
+
+console.log(playRound(playerSelection, computerSelection));
