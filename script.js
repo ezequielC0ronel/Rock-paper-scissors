28fdffc165a305a391afd1c71f5b79f3a527ad57
Ezequiel Coronel <nikocor99@gmail.com>
2021-10-28 16:23:31 -0300

    Added playerscore and computer score, added game function

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 let valuesArray = ["Rock", "Paper", "Scissors"];
+let playerScore = 0;
+let computerScore = 0;
 
 function computerPlay() {
   let value = Math.floor(Math.random() * 3);
@@ -8,17 +10,42 @@ function computerPlay() {
 function playRound(playerSelection, computerSelection) {
   playerSelection = playerSelection.toLowerCase();
   computerSelection = computerSelection.toLowerCase();
+  let finalResult;
 
-  if (
+  if (playerSelection === computerSelection) {
+    finalResult = `Tie game!`;
+  } else if (
     (computerSelection == "rock" && playerSelection == "scissors") ||
     (computerSelection == "scissors" && playerSelection == "paper") ||
     (computerSelection == "paper" && playerSelection == "rock")
   ) {
-    return `You Lose! ${computerSelection} beats ${playerSelection}`;
+    finalResult = `You Lose! ${computerSelection} beats ${playerSelection}`;
+    computerScore++;
+  } else {
+    finalResult = `You Win! ${playerSelection} beats ${computerSelection}`;
+    playerScore++;
+  }
+  return finalResult;
+}
+
+function game() {
+  let round = 1;
+  while (round <= 5) {
+    console.log(`Round ${round}`);
+    let playerPick = prompt("Make a selection!");
+    let computerPick = computerPlay();
+    console.log(playRound(playerPick, computerPick));
+    console.log(`User:${playerScore}; Cpu:${computerScore};`);
+    round++;
+  }
+
+  if(playerScore === computerScore){
+    console.log("No Winners! Tie game");
+  }else if (playerScore > computerScore){
+    console.log("User Wins!");
+  }else if (computerScore > playerScore){
+    console.log("CPU Wins!");
   }
 }
 
-const playerSelection = "rock";
-const computerSelection = computerPlay();
-
-console.log(playRound(playerSelection, computerSelection));
+game();
